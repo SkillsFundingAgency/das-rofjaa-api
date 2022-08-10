a5ef133ac6ccc17662d8b3bd96bbc1ab8110dc05
Alasdair Reid <ali.reid@live.co.uk>
2022-08-10 10:54:25 +0100

    Removal table fields

--- src/SFA.DAS.Rofjaa.Database/Tables/Agency.sql
+++ src/SFA.DAS.Rofjaa.Database/Tables/Agency.sql
@@ -3,8 +3,6 @@
 	[LegalEntityId] bigint NOT NULL,
 	[IsGrantFunded] bit NOT NULL DEFAULT 0,
 	[EffectiveFrom] DATETIME2,
-	[EffectiveTo] DATETIME2 NULL,
-	[RemovalReason] NVARCHAR(MAX) NULL,
 	[CreatedDate] DATETIME2 DEFAULT GETDATE(),
 	[LastUpdatedDate] DATETIME2 NULL,
 	CONSTRAINT [PK_Agency] PRIMARY KEY ([LegalEntityId], [EffectiveFrom])
