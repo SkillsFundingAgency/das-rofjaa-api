8ede39b48712f6553246c3ce5071e06246c67fe9
Alasdair Reid <ali.reid@live.co.uk>
2022-09-12 12:01:35 +0100

    EffectiveFrom not nullable

--- src/SFA.DAS.Rofjaa.Database/Tables/Agency.sql
+++ src/SFA.DAS.Rofjaa.Database/Tables/Agency.sql
@@ -2,7 +2,7 @@
 (
 	[LegalEntityId] bigint NOT NULL,
 	[IsGrantFunded] bit NOT NULL DEFAULT 0,
-	[EffectiveFrom] DATETIME2 NULL,
+	[EffectiveFrom] DATETIME2 DEFAULT GETDATE(),
 	[CreatedDate] DATETIME2 DEFAULT GETDATE(),
 	[LastUpdatedDate] DATETIME2 NULL,
 	CONSTRAINT [PK_Agency] PRIMARY KEY ([LegalEntityId], [EffectiveFrom])
