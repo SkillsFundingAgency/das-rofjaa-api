6ddd808418c043d4a7d6c79450088e52f6efd340
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-18 08:50:15 +0100

    Fixing failing acceptance tests

--- src/SFA.DAS.Rofjaa.Api.AcceptanceTests/Steps/AgenciesSteps.cs
+++ src/SFA.DAS.Rofjaa.Api.AcceptanceTests/Steps/AgenciesSteps.cs
@@ -6,44 +6,43 @@ using SFA.DAS.Rofjaa.Api.AcceptanceTests.Infrastructure;
 using SFA.DAS.Rofjaa.Api.ApiResponses;
 using TechTalk.SpecFlow;
 
-namespace SFA.DAS.Rofjaa.Api.AcceptanceTests.Steps
+namespace SFA.DAS.Rofjaa.Api.AcceptanceTests.Steps;
+
+[Binding]
+public class AgencySteps
 {
-    [Binding]
-    public class AgencySteps
+    private readonly ScenarioContext _context;
+
+    public AgencySteps(ScenarioContext context)
     {
-        private readonly ScenarioContext _context;
+        _context = context;
+    }
 
-        public AgencySteps(ScenarioContext context)
+    [Then("all agencies are returned")]
+    public async Task ThenAllAgenciesReturned()
+    {
+        if (!_context.TryGetValue<HttpResponseMessage>(ContextKeys.HttpResponse, out var result))
         {
-            _context = context;
+            Assert.Fail($"scenario context does not contain value for key [{ContextKeys.HttpResponse}]");
         }
 
-        [Then("all agencies are returned")]
-        public async Task ThenAllAgenciesReturned()
+        var model = await HttpUtilities.ReadContent<GetAgenciesResponse>(result.Content);
+        var expected = DbUtilities.GetAgencies();
+
+        expected.Should().BeEquivalentTo(model.Agencies);
+    }
+
+    [Then("the agency with id equal to 1 is returned")]
+    public async Task ThenAgencyIsReturned()
+    {
+        if (!_context.TryGetValue<HttpResponseMessage>(ContextKeys.HttpResponse, out var result))
         {
-            if (!_context.TryGetValue<HttpResponseMessage>(ContextKeys.HttpResponse, out var result))
-            {
-                Assert.Fail($"scenario context does not contain value for key [{ContextKeys.HttpResponse}]");
-            }
-
-            var model = await HttpUtilities.ReadContent<GetAgenciesResponse>(result.Content);
-            var expected = DbUtilities.GetAgencies();
-
-            model.Agencies.Should().BeEquivalentTo(expected);
+            Assert.Fail($"scenario context does not contain value for key [{ContextKeys.HttpResponse}]");
         }
 
-        [Then("the agency with id equal to 1 is returned")]
-        public async Task ThenAgencyIsReturned()
-        {
-            if (!_context.TryGetValue<HttpResponseMessage>(ContextKeys.HttpResponse, out var result))
-            {
-                Assert.Fail($"scenario context does not contain value for key [{ContextKeys.HttpResponse}]");
-            }
+        var model = await HttpUtilities.ReadContent<GetAgencyResponse>(result.Content);
+        var expected = DbUtilities.GetAgency(1);
 
-            var model = await HttpUtilities.ReadContent<GetAgencyResponse>(result.Content);
-            var expected = DbUtilities.GetAgency(1);
-
-            model.Should().BeEquivalentTo(expected);
-        }
+        expected.Should().BeEquivalentTo(model);
     }
 }
