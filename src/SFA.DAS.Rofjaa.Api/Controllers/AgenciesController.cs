056ce860e29b6b1647871f2f4fa2f792cb54891c
ogrwill <will.ogrady@digital.education.gov.uk>
2024-07-22 15:44:15 +0100

    remove kibana and add test logging

--- src/SFA.DAS.Rofjaa.Api/Controllers/AgenciesController.cs
+++ src/SFA.DAS.Rofjaa.Api/Controllers/AgenciesController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Rofjaa.Api.ApiResponses;
 using SFA.DAS.Rofjaa.Application.Agencies.Queries.GetAgencies;
 using SFA.DAS.Rofjaa.Application.Agencies.Queries.GetAgency;
@@ -11,12 +12,13 @@ namespace SFA.DAS.Rofjaa.Api.Controllers;
 [ApiVersion("1.0")]
 [ApiController]
 [Route("agencies/")]
-public class AgenciesController(IMediator mediator) : ControllerBase
-{
+public class AgenciesController(IMediator mediator, ILogger<AgenciesController> logger) : ControllerBase
+{  
     [HttpGet]
     [Route("{legalEntityId}")]
     public async Task<IActionResult> Get(long legalEntityId)
     {
+        logger.LogInformation($"Get called for legalEntityId {legalEntityId}");
         var result = await mediator.Send(new GetAgencyQuery { LegalEntityId = legalEntityId });
 
         if (result == null) return NotFound();
@@ -29,6 +31,7 @@ public class AgenciesController(IMediator mediator) : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetList()
     {
+        logger.LogInformation("GetList called");
         var result = await mediator.Send(new GetAgenciesQuery());
 
         var response = new GetAgenciesResponse
